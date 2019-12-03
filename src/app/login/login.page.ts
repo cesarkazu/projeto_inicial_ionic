e956937a7daf8143519269ee30968384dd17a583
Cesar Kazuyoshi Ando <ckando@stefanini.com>
2019-12-03 10:29:20 -0300

    cadastro

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms'
+
+import { Autenticacao } from '../services/auth/autenticacao.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  providers: [ Autenticacao ]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null),
+    'senha': new FormControl(null)
+  })
+
+  constructor(
+    private autenticacao: Autenticacao
+  ) { }
 
   ngOnInit() {
   }
 
+  public autenticar(): void{
+    this.autenticacao.autenticar(
+      this.formulario.value.email,
+      this.formulario.value.senha
+    )
+  }
 }
